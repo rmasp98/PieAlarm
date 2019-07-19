342ec3042de78e1aca3555c1457d9e9abda68144
Ross Maspero <rossmaspero@gmail.com>
2019-07-19 19:34:18 +0100

    Started making alarm but transferring to other machine

---
+++ Alarm/alarm.py
@@ -0,0 +1,8 @@
+
+
+class Alarm:
+    def __init__(self):
+        pass
+        self.time = "TODO find time type"
+        self.days = ["days"]
+        self.trigger = "What will happen at alarm"
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from ui.ui_controller import UiController
 
 if __name__ == "__main__":
     ui = UiController()
-    ui.window.set_dark()
+    ui.window.set_weather((22, "sunny") for i in range(5))
+    # ui.window.set_dark()
     ui.exec()
