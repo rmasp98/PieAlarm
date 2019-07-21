930494c86629856355107e410da78a5c7453be12
Ross Maspero <rossmaspero@gmail.com>
2019-07-21 23:36:50 +0100

    Learning vscode git controls

--- Alarm/alarm.py
+++
@@ -1,8 +0,0 @@
-
-
-class Alarm:
-    def __init__(self):
-        pass
-        self.time = "TODO find time type"
-        self.days = ["days"]
-        self.trigger = "What will happen at alarm"
--- main.py
+++
@@ -1,7 +0,0 @@
-from ui.ui_controller import UiController
-
-if __name__ == "__main__":
-    ui = UiController()
-    ui.window.set_weather((22, "sunny") for i in range(5))
-    # ui.window.set_dark()
-    ui.exec()
